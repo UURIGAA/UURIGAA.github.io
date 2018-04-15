96d4c95b4f941b15995dda8e887c1caae9de2d17
UURIGAA <1297879533@qq.com>
2018-04-15 15:36:51 +0800

    Site updated: 2018-04-15 15:36:43

--- js/src/custom/custom.js
+++ js/src/custom/custom.js
@@ -1,3 +1,3 @@
-// build time:Thu Apr 12 2018 19:47:12 GMT+0800 (中国标准时间)
+// build time:Sat Apr 14 2018 08:18:30 GMT+0800 (中国标准时间)
 $(document).ready(function(){$(window).scroll(function(){$(".top-scroll-bar").attr("style","width: "+$(this).scrollTop()/($(document).height()-$(this).height())*100+"%; display: block;");var c=$(window).scrollTop();var o=$(document).height();var r=$(window).height();var s=parseInt(c/(o-r)*100);$(".top-scroll-bar").css("width",s+"%");if(s>=0&&s<=19)$(".top-scroll-bar").css("background","#cccccc");if(s>=20&&s<=39)$(".top-scroll-bar").css("background","#50bcb6");if(s>=40&&s<=59)$(".top-scroll-bar").css("background","#85c440");if(s>=60&&s<=79)$(".top-scroll-bar").css("background","#f2b63c");if(s>=80&&s<=99)$(".top-scroll-bar").css("background","#FF0000");if(s==100)$(".top-scroll-bar").css("background","#f58ca1")})});
 //rebuild by neat 
--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Thu Apr 12 2018 19:47:11 GMT+0800 (中国标准时间)
+// build time:Sat Apr 14 2018 08:18:29 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
